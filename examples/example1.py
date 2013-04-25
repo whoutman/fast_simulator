c6c129ae8eb54bf60663e9ea7d7ce7d5349d259f
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2013-04-25 20:43:09 +0000

    Updated scenario to cleanup
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/tue_simulator/fast_simulator@15218 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- examples/example1.py
+++ examples/example1.py
@@ -9,9 +9,27 @@ if __name__ == "__main__":
 
     W = client.SimWorld()
 
-    person1 = W.add_object("loy", "person", 2.5, 0.1, 0)
-    coke = W.add_object("coke-1", "coke")
-    coke.set_position(4, 1, 0)
+    #person1 = W.add_object("loy", "person", 2.5, 0.1, 0)
+    #coke = W.add_object("coke-1", "coke")
+    #coke.set_position(4, 1, 0)
 
-    raw_input("Press Enter to continue...")
-    person1.set_position(2, 1.9, 0)
+    next_step = False
+    while not rospy.is_shutdown() and not next_step:
+    	rospy.sleep(0.5)
+    	if W.amigo_speech_contains("Which room") or W.amigo_speech_contains("What room"):
+    		rospy.sleep(2.0)
+    		W.speak("livingroom", "/speech_room/output")
+    		next_step = True
+
+    next_step = False
+    while not rospy.is_shutdown() and not next_step:
+    	rospy.sleep(0.5)
+    	if W.amigo_speech_contains("Am I right"):
+    		rospy.sleep(2.0)
+    		W.speak("yes", "/speech_yesno/output")
+    		next_step = True
+    
+    
+
+    #raw_input("Press Enter to continue...")
+    #person1.set_position(2, 1.9, 0)
