85e81a2a937bfeea7c446c1c6d7e9ca1437c1372
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2013-04-26 08:22:05 +0000

    Added console
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/tue_simulator/fast_simulator@15260 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

---
+++ examples/example1.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+import roslib; roslib.load_manifest('fast_simulator')
+import rospy
+
+from fast_simulator import client
+
+if __name__ == "__main__":
+    rospy.init_node('gazebo_life_example_1')
+
+    W = client.SimWorld()
+
+    #person1 = W.add_object("loy", "person", 2.5, 0.1, 0)
+    coke = W.add_object("coke-1", "coke", 1.450, 1.329, 1.0)
+    #coke.set_position(4, 1, 0)
+
+    next_step = False
+    while not rospy.is_shutdown() and not next_step:
+    	rospy.sleep(0.5)
+    	if W.amigo_speech_contains("Which room") or W.amigo_speech_contains("What room"):
+    		rospy.sleep(2.0)
+    		W.speak("livingroom", "/speech_room/output")
+    		next_step = True
+
+    next_step = False
+    while not rospy.is_shutdown() and not next_step:
+    	rospy.sleep(0.5)
+    	if W.amigo_speech_contains("Am I right"):
+    		rospy.sleep(2.0)
+    		W.speak("yes", "/speech_yesno/output")
+    		next_step = True
+
+    
+
+    #raw_input("Press Enter to continue...")
+    #person1.set_position(2, 1.9, 0)
---
+++ src/fast_simulator/console.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+import roslib; roslib.load_manifest('fast_simulator')
+import rospy
+
+from fast_simulator import client
+
+if __name__ == "__main__":
+    rospy.init_node('fast_simulator_console')
+    world = client.SimWorld()
